5ac20f37cfb826702d13a3a3ced3d0a293f35e7d
Şeyma Büyükaydın <seymabuyukaydin7@gmail.com>
2025-02-28 12:15:25 +0300

    Add files via upload

---
+++ week-2-SheimaShey/src/Dt.java
@@ -0,0 +1,13 @@
+public class Dt {
+    public static void main(String[] args) {
+        int grade = 1;
+        double grade2 = 1.5;
+        boolean isOk= true;
+        char first = 'A';
+
+        System.out.println(grade);
+        System.out.println(grade2);
+        System.out.println(isOk);
+        System.out.println(first);
+    }
+}
